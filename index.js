0ff73a812b328e52619f910fae6ce076a0d8c4c8
brapoprod <broels@chili-publish.com>
2022-12-20 19:56:48 +0100

    Remove unnecessary output

--- index.js
+++ index.js
@@ -94,10 +94,6 @@ const main = async () => {
         await getPrs();
         core.setOutput("jiraTickets", jiraTickets);
 
-      
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        console.log(`The event payload: ${payload}`);
       } catch (error) {
         core.setFailed(error.message);
       }      
